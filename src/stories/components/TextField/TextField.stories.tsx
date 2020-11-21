0400661b6d893499cdb6ca373658deffd08cc6ca
Daniel Watson <dwatson251@gmail.com>
2020-11-21 23:35:34 +0000

    Initial commit

---
+++ src/stories/components/TextField/TextField.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+import { TextField } from './TextField';
+
+export default {
+    title: 'Components/TextField',
+    component: TextField,
+} as Meta;
+
+export const Primary: React.FunctionComponent<{}> = () => <TextField primary>Button</TextField>;
---
+++ src/stories/components/TextField/TextField.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+/**
+ * Primary UI component for user interaction
+ */
+export const TextField = () => {
+    return (
+        <input
+            type="text"
+            // placeholder={label}
+        />
+    );
+};
